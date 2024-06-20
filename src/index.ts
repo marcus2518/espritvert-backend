6fe224edc14932ae9c8d760c6d8f2933ca883a6e
Marcus Phan <marcusphan1@gmail.com>
2024-06-20 14:10:43 -0400

    Attempt to fix docker?

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
