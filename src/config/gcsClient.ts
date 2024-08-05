50f53aa38ef81dc600bf83ffdaf0f60d440152aa
Marcus Phan <marcusphan@Marcuss-MacBook-Pro.local>
2024-08-05 19:56:20 -0400

    using ADC

--- src/config/gcsClient.ts
+++ src/config/gcsClient.ts
@@ -3,13 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const storage = new Storage({
-    projectId: process.env.GCLOUD_PROJECT_ID,
-    credentials: {
-        private_key: process.env.GCLOUD_PRIVATE_KEY,
-        client_email: process.env.GCLOUD_CLIENT_EMAIL,
-    },
-});
+const storage = new Storage();
 
 const bucket = storage.bucket("posting_images");
 
